0e31814f49a39f094ce22b4218460ff949824737
Joshua Greer <jjgreer@asu.edu>
2024-03-24 20:53:57 -0700

    re templated because bad naming conventions
    bruh

---
+++ Assignment4Part3/Assignment4Part3/Assignment4Part3/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Assignment4Part3.Models;
+using Assignment4Part3.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Assignment4Part3.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
