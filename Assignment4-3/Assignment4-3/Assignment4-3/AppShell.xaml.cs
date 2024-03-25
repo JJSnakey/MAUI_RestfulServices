5d761f9bac93a1aa9d276ac73ab4e9252e1c4ede
Joshua Greer <jjgreer@asu.edu>
2024-03-24 20:44:22 -0700

    template android emulator
    oh boy

---
+++ Assignment4-3/Assignment4-3/Assignment4-3/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Assignment4_3.ViewModels;
+using Assignment4_3.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Assignment4_3
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
